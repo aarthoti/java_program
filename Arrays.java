86381d93607ab813f0542dcadc8b6b319ccb317e
aarthoti <165478144+aarthoti@users.noreply.github.com>
2024-11-08 16:20:41 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Arrays{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int i,n;
+int a[]=new int[n];
+System.out.println("The element in to arrays:");
+for(int i=0;i<a.length;i++)
+{
+a[i]=sc.nextInt();
+}
+System.out.println("The element in an arrays is:");
+for(int i=0;i<a.length;i++)
+{
+System.out.println(a[i]);
+}
+}
+}
